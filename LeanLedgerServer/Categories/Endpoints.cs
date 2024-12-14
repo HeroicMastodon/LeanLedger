9ef990ad0ecb40b82536d438da60d854fc0d2f36
HeroicMastodon <heroicmastodon@gmail.com>
2024-12-14 00:29:40 -0700

    Backend endpoints

--- LeanLedgerServer/Categories/Endpoints.cs
+++ LeanLedgerServer/Categories/Endpoints.cs
@@ -3,12 +3,41 @@ namespace LeanLedgerServer.Categories;
 using Common;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Transactions;
 using static Results;
 
 public static class Endpoints {
     public static void MapCategories(this IEndpointRouteBuilder endpoints) {
-        var transactions = endpoints.MapGroup("categories");
-        transactions.MapGet("options", ListCategoryOptions);
+        var categories = endpoints.MapGroup("categories");
+        categories.MapGet("options", ListCategoryOptions);
+        categories.MapGet("", ListCategories);
+        categories.MapGet("{category}", GetCategory);
+    }
+
+    private static async Task<IResult> GetCategory(
+        string category,
+        [FromServices]
+        LedgerDbContext dbContext
+    ) {
+        var transactions = await dbContext.Transactions
+            .Where(t => t.Type == TransactionType.Expense)
+            .Where(t => t.Category == category)
+            .ToListAsync();
+
+        return Ok(new { Category = category, Transactions = transactions, });
+    }
+
+    private static async Task<IResult> ListCategories(
+        [FromServices]
+        LedgerDbContext dbContext
+    ) {
+        var transactions = await dbContext.Transactions
+            .Where(t => t.Type == TransactionType.Expense)
+            .GroupBy(t => t.Category)
+            .Select(g => new { Category = g.Key, Amount = g.Select(t => t.Amount).Sum() })
+            .ToListAsync();
+
+        return Ok(transactions);
     }
 
     private static async Task<IResult> ListCategoryOptions(
