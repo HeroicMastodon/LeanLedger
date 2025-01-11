3ab30035042b4491356aa8122eb72b1ee0254555
HeroicMastodon <heroicmastodon@gmail.com>
2025-01-11 00:38:22 -0700

    List matching transactions for rule endpoint

--- LeanLedgerServer/Automation/RulesController.cs
+++ LeanLedgerServer/Automation/RulesController.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace LeanLedgerServer.Automation;
 
+using System.Diagnostics;
 using AutoMapper;
 using Common;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using Transactions;
 
 [ApiController]
 [Route("api/[controller]")]
@@ -85,6 +88,82 @@ public class RulesController(
 
         return NoContent();
     }
+
+    [HttpGet("{id:guid}/matching")]
+    public async Task<IActionResult> GetMatchingTransactions(
+        Guid id,
+        [FromQuery(Name = "start")]
+        DateOnly startDate,
+        [FromQuery(Name = "end")]
+        DateOnly endDate,
+        [FromQuery(Name = "limit")]
+        int limit = 5
+    ) {
+        var rule = await dbContext.Rules.FindAsync(id);
+
+        if (rule is null) {
+            return NotFound();
+        }
+
+        var results = await FindMatchingTransactionsForRule(rule, startDate, endDate, limit);
+
+        return Ok(results);
+    }
+
+    private async Task<List<Transaction>> FindMatchingTransactionsForRule(
+        Rule rule,
+        DateOnly startDate,
+        DateOnly endDate,
+        int? limit = null
+    ) {
+        var queryString = "select * from transactions";
+        List<SqliteParameter?> values = [];
+        foreach (var ((field, not, condition, value), index) in rule.Triggers.Enumerate()) {
+            var fieldName = field switch {
+                TransactionRuleField.Description => "Description",
+                TransactionRuleField.Date => "Date",
+                TransactionRuleField.Amount => "Amount",
+                TransactionRuleField.Type => "Type",
+                TransactionRuleField.Category => "Category",
+                TransactionRuleField.Source => "SourceAccountId",
+                TransactionRuleField.Destination => "DestinationAccountId",
+                _ => throw new UnreachableException()
+            };
+            var valueName = $"@value{index}";
+            var comparison = condition switch {
+                RuleCondition.StartsWith => $"{NotToString(not)} like {valueName} + '%'",
+                RuleCondition.EndsWith => $"{NotToString(not)} like '%' + {valueName}",
+                RuleCondition.Contains => $"{NotToString(not)} like '%' + {valueName} + '%'",
+                RuleCondition.IsExactly => $"{NotToString(not)} like {valueName}",
+                RuleCondition.GreaterThan => $"{(not ? "<=" : ">")} {valueName}",
+                RuleCondition.LessThan => $"{(not ? ">=" : "<")} {valueName}",
+                RuleCondition.Exists => $"is {NotToString(!not)} null",
+                _ => throw new UnreachableException()
+            };
+            var prefix = index == 0 ? "where" : "and";
+
+            queryString += $" {prefix} {fieldName} {comparison}";
+            values.Add(new SqliteParameter(valueName, value));
+        }
+
+        // avoiding some weird covariant error I guess
+        var valuesArray = values.Select(object? (v) => v).ToArray();
+        var query = dbContext
+            .Transactions
+            .FromSqlRaw(queryString, valuesArray)
+            .Where(t => t.Date >= startDate && t.Date <= endDate);
+
+        if (limit is not null) {
+            query = query.Take(limit.Value);
+        }
+
+        var results = await query.ToListAsync();
+
+        return results;
+    }
+
+
+    private static string NotToString(bool? not) => not == true ? "not" : "";
 }
 
 public record RuleRequest(
