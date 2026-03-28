3bdff70f048b70b81a1316595935fc08f007f8e2
HeroicMastodon <heroicmastodon@gmail.com>
2026-03-28 16:12:45 -0600

    some formatting

--- LeanLedgerApp/src/lib/metrics/NetWorth.svelte
+++ LeanLedgerApp/src/lib/metrics/NetWorth.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
     import Card from "$lib/components/Card.svelte";
-    import {apiClient} from "$lib/apiClient";
-    import {monthManager} from "$lib/selectedMonth.svelte";
-    import {ProgressBar} from "@skeletonlabs/skeleton";
+    import { apiClient } from "$lib/apiClient";
+    import { monthManager } from "$lib/selectedMonth.svelte";
+    import { ProgressBar } from "@skeletonlabs/skeleton";
     import Money from "$lib/components/Money.svelte";
+
     type NetWorth = {
-            netWorth: number;
-            income: number;
-            expenses: number;
-            yearOverYear: number;
-        };
+        netWorth: number;
+        income: number;
+        expenses: number;
+        yearOverYear: number;
+    };
 
     const netWorthPromise = $derived.by(async () => {
-        const resp = await apiClient.get<NetWorth>(`Metrics/net-worth?${monthManager.params}`);
+        const resp = await apiClient.get<NetWorth>(
+            `Metrics/net-worth?${monthManager.params}`,
+        );
 
         return resp.data;
     });
 </script>
-
 
 {#await netWorthPromise}
     <Card class="grow lg:flex-initial">
@@ -27,12 +29,14 @@
     </Card>
 {:then netWorth}
     <Card class="grow lg:flex-initial">
-        <h2 class="h2">Net Worth: <Money  amount={netWorth.netWorth} /></h2>
+        <h2 class="h2">Net Worth: <Money amount={netWorth.netWorth} /></h2>
         <hr class="hr mb-4" />
         <div class="grid grid-cols-2 gap-4">
-            <div>In: <Money amount= {netWorth.income} /> </div>
-            <div>Change: <Money amount={netWorth.income - netWorth.expenses} /> </div>
-            <div>Out: <Money amount={netWorth.expenses} type="Expense"/></div>
+            <div>In: <Money amount={netWorth.income} /></div>
+            <div>
+                Change: <Money amount={netWorth.income - netWorth.expenses} />
+            </div>
+            <div>Out: <Money amount={netWorth.expenses} type="Expense" /></div>
             <div>YoY Change: <Money amount={netWorth.yearOverYear} /></div>
         </div>
     </Card>
--- LeanLedgerApp/tailwind.config.ts
+++ LeanLedgerApp/tailwind.config.ts
@@ -2,30 +2,33 @@ import { join } from 'path'
 import type { Config } from 'tailwindcss'
 import forms from '@tailwindcss/forms';
 import { skeleton } from '@skeletonlabs/tw-plugin'
-import {carbonFox} from './carbon-fox';
+import { carbonFox } from './carbon-fox';
 
 export default {
-	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		forms,
-		skeleton({
-			themes: {
+    darkMode: 'class',
+    content: [
+        './src/**/*.{html,js,svelte,ts}',
+        join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}'),
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        forms,
+        skeleton({
+            themes: {
                 custom: [
                     carbonFox,
                 ],
-				preset: [
-					{
-						name: 'wintry',
-						enhancements: true,
-					},
-				],
-			},
-		}),
-	],
+                preset: [
+                    {
+                        name: 'wintry',
+                        enhancements: true,
+                    },
+                ],
+            },
+        }),
+    ],
     safelist: [
         "variant-outline-error",
         "variant-outline-primary",
